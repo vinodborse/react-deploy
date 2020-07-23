7b5e27f1614ba45b8f62da9000f9f6493ba6903b
vinod <borse002@gmail.com>
2020-07-23 15:54:07 +0530

    Updates

---
+++ precache-manifest.5510a0fef9dab5dcb5c512d4572913b5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65d7dd7ca27b33a469b6f17f62ce8e3a",
+    "url": "/react-deploy/index.html"
+  },
+  {
+    "revision": "20ed621a89921bab1228",
+    "url": "/react-deploy/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "340aac5933c756125f93",
+    "url": "/react-deploy/static/js/2.a0c31166.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-deploy/static/js/2.a0c31166.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "20ed621a89921bab1228",
+    "url": "/react-deploy/static/js/main.08a97474.chunk.js"
+  },
+  {
+    "revision": "9a6b4006b58ad85b50f6",
+    "url": "/react-deploy/static/js/runtime-main.3e11c617.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-deploy/static/media/logo.5d5d9eef.svg"
+  }
+]);
